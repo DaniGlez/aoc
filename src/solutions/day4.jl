1e8e487ac3b8efc6050c85de47f423af820021f2
Daniel González <daniel.gonzalez@ienai.space>
2022-12-04 09:21:08 +0100

    Clean-up naming

--- src/solutions/day4.jl
+++ src/solutions/day4.jl
@@ -7,8 +7,8 @@ lines = split(readchomp("src/inputs/day4.txt"), "\n")
 pairs = range_pair.(lines)
 
 # p1
-pair_contained(pair1, pair2) = (pair1[begin] ∈ pair2) && (pair1[end] ∈ pair2)
-matches_p1(pair) = pair_contained(pair[1], pair[2]) || pair_contained(pair[2], pair[1])
+range_contained(r1, r2) = (r1[begin] ∈ r2) && (r1[end] ∈ r2)
+matches_p1(pair) = range_contained(pair[1], pair[2]) || range_contained(pair[2], pair[1])
 @pipe matches_p1.(pairs) .|> Int64 |> sum
 
 # p2
