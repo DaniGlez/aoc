9aba11ee5884ef3b864d971f6cc016967b1c6fd9
Daniel González <daniel.gonzalez@ienai.space>
2022-12-06 11:17:24 +0100

    Add variation with allunique

--- src/solutions/day6.jl
+++ src/solutions/day6.jl
@@ -5,3 +5,5 @@ findndiff(msg, n) = findfirst(i -> length(Set(msg[i:i+n-1])) == n, 1:length(msg)
 findndiff(datastream, 4) |> println
 findndiff(datastream, 14) |> println
 
+# Better version of findndiff, TIL about this function
+findndiff(msg, n) = findfirst(i -> allunique(msg[i:i+n-1]), 1:length(msg)) + n - 1
