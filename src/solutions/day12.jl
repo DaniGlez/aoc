34f6451edbb8be6dfbd5e6c79bec90271c3076a1
Daniel González <daniel.gonzalez@ienai.space>
2022-12-12 22:40:18 +0100

    MOAR efficiency

--- src/solutions/day12.jl
+++ src/solutions/day12.jl
@@ -1,4 +1,4 @@
-using DataStructures, Pipe
+using DataStructures, Pipe, StaticArrays
 const CI = CartesianIndex
 # parsing
 line2vec(line) = (collect ∘ strip)(line) .- 'a'
@@ -17,30 +17,32 @@ end
 
 l∞(ci::CartesianIndex) = abs(ci.I[1]) + abs(ci.I[2])
 
-const directions = (CI(1, 0), CI(0, 1), CI(-1, 0), CI(0, -1))
+const directions = @SArray [CI(1, 0), CI(0, 1), CI(-1, 0), CI(0, -1)]
 _in_grid(A, ci) = (ci.I[1] ∈ 1:size(A)[1]) && (ci.I[2] ∈ 1:size(A)[2])
 crossing_allowed(A, from, to) = (A[to] <= A[from] + 1)
-valid_neighbour(A, from, to) = _in_grid(A, to) && crossing_allowed(A, from, to)
-valid_neighbour_bwd(A, from, to) = _in_grid(A, from) && crossing_allowed(A, from, to)
-_neighbours(ci) = [(ci + dir) for dir ∈ directions]
-neighbours_fwd(A, ci) = filter(n -> valid_neighbour(A, ci, n), _neighbours(ci))
-neighbours_bwd(A, ci) = filter(n -> valid_neighbour_bwd(A, n, ci), _neighbours(ci))
+valid_neighbour_fwd(A, from, to) = _in_grid(A, to) && crossing_allowed(A, from, to)
+valid_neighbour_bwd(A, from, to) = _in_grid(A, to) && crossing_allowed(A, to, from)
 
-function search(A, start, neighbours, finish_cond, heuristic)
-    S = similar(A) # steps from start
-    fill!(S, -1)
-    S[start] = 0
-    frontier = PriorityQueue{CartesianIndex{2},Int64}()
-    enqueue!(frontier, start => 0)
-    while true
-        current, _ = dequeue_pair!(frontier)
-        finish_cond(current) && (return S[current])
-        for n ∈ neighbours(A, current)
-            steps = S[current] + 1
-            if (S[n] == -1) || (steps < S[n])
-                S[n] = steps
-                prio = steps + heuristic(n)
-                (n ∈ keys(frontier)) ? (frontier[n] = prio) : enqueue!(frontier, n => prio)
+using Base.Cartesian: @nexprs
+@generated function search(A, start, valid_neighbour, finish_cond)
+    quote
+        S = similar(A) # steps from start
+        fill!(S, -1)
+        S[start] = 0
+        frontier = Queue{CartesianIndex{2}}()
+        enqueue!(frontier, start)
+        while true
+            current = dequeue!(frontier)
+            finish_cond(current) && (return S[current])
+            @nexprs 4 i -> begin
+                n = current + directions[i]
+                if valid_neighbour(A, current, n)
+                    steps = S[current] + 1
+                    if (S[n] == -1) || (steps < S[n])
+                        S[n] = steps
+                        enqueue!(frontier, n)
+                    end
+                end
             end
         end
     end
@@ -48,7 +50,7 @@ end
 
 function solve_p1()
     A, start, finish = inputs()
-    search(A, start, neighbours_fwd, x -> (x == finish), x -> l∞(x - finish))
+    search(A, start, valid_neighbour_fwd, x -> (x == finish))
 end
 
 solve_p1() |> println
@@ -56,6 +58,6 @@ solve_p1() |> println
 # part 2: BFS
 function solve_p2()
     A, _, finish = inputs()
-    search(A, finish, neighbours_bwd, x -> (A[x] == 0), x -> 0)
+    search(A, finish, valid_neighbour_bwd, x -> (A[x] == 0))
 end
 solve_p2() |> println
