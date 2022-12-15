7db36ea012bac8c12aa476eaf8183b3fd5f20f1a
Daniel González <daniel.gonzalez@ienai.space>
2022-12-15 14:54:36 +0100

    Add Day 15 part 2

--- src/solutions/day15.jl
+++ src/solutions/day15.jl
@@ -24,15 +24,38 @@ function add_exclusion_interval(intervals, sensor, radius, y)
     intervals ∪ IntervalSet(exclusion_interval)
 end
 
-function solve_p1(sbr, line_y)
+
+function exclusion_intervals(sbr, line_y)
     intervals = IntervalSet()
     for (sensor, beacon, radius) ∈ sbr
         intervals = add_exclusion_interval(intervals, sensor, radius, line_y)
     end
-    intervals.items .|> span |> sum
+    intervals
 end
+
+total_span(intset::IntervalSet) = intset.items .|> span |> sum
+solve_p1(sbr, line_y) = exclusion_intervals(sbr, line_y) |> total_span
 
 sbr = parse_input("src/inputs/day15.txt")
 solve_p1(sbr, 2_000_000) |> println
+
+function _solve_p2(sbr, max_coord=4_000_000)
+    grid_interval = IntervalSet(1 .. max_coord)
+    for i ∈ 1:max_coord
+        excl = exclusion_intervals(sbr, i) ∩ grid_interval
+        if total_span(excl) < max_coord - 1
+            println(total_span(excl))
+            return (i, setdiff(grid_interval, excl))
+        end
+    end
+end
+
+function solve_p2(inputs)
+    y, intset = _solve_p2(inputs)
+    y + 4_000_000(intset.items[1].first + 1)
+end
+
+solve_p2(sbr) |> println
+
 
 
