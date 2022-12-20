3f5ddb3ca2d9b5deed1b359a3b2a4f834e74e87a
Daniel González <daniel.gonzalez@ienai.space>
2022-12-20 15:17:39 +0100

    Remove unnecessary functions

--- src/solutions/day20.jl
+++ src/solutions/day20.jl
@@ -8,27 +8,17 @@ mutable struct WrappyVector{N,T}
     successors::Vector{T}
     predecessors::Vector{T}
     zero_idx::T
-    first::T
     function WrappyVector(values::Vector{T}) where {T}
         N = length(values)
         successors = T.(mod1.((2:(N+1)), N))
         predecessors = T.(mod1.((N:(2N-1)), N))
         zero_idx = findfirst(v -> v == 0, values)
-        new{N,T}(values, successors, predecessors, zero_idx, one(T))
+        new{N,T}(values, successors, predecessors, zero_idx)
     end
 end
 
 s_or_p(w::WrappyVector, s) = (s < 0) ? w.predecessors : w.successors
 neighbours(w, i) = (w.predecessors[i], w.successors[i])
-
-function jump(vector, i, n_jumps, ji)
-    ji_crosses = 0
-    for _ ∈ 1:n_jumps
-        (i == ji) && (ji_crosses += 1)
-        i = vector[i]
-    end
-    i, ji_crosses
-end
 
 function jump(vector, i, n_jumps)
     for _ ∈ 1:n_jumps
@@ -37,24 +27,14 @@ function jump(vector, i, n_jumps)
     i
 end
 
-function shift_origin!(w::WrappyVector, s)
-    (s == 1) && (w.first = w.successors[w.first])
-    (s == -1) && (w.first = w.predecessors[w.first])
-end
-
 function shift!(w::WrappyVector{N,T}) where {N,T}
     for (i, v) ∈ enumerate(w.values)
-        #printw(w)
         v == 0 && continue
         s = sign(v)
         m = abs(v)
-        cyclic_shifts = (m ÷ (N - 1)) % N
         side = s_or_p(w, s)
-        w.first = jump(side, w.first, cyclic_shifts)
         rem_shift = m % (N - 1)
-        cross_index = w.first
-        (s == -1) && (cross_index = w.predecessors[cross_index])
-        j, origin_crosses = jump(side, i, rem_shift, cross_index)
+        j = jump(side, i, rem_shift)
         p_i, s_i = neighbours(w, i)
         p_j, s_j = neighbours(w, j)
         w.successors[p_i] = s_i
@@ -70,19 +50,6 @@ function shift!(w::WrappyVector{N,T}) where {N,T}
             w.successors[i] = j
             w.predecessors[j] = i
         end
-        #println("i=$i, j=$j, s=$s, fi=$(w.first)")
-        #print("    Post reweave: ")
-        #printw(w)
-        if (w.first == i)
-            (s > 0) && (w.first = s_i)
-            (s < 0) && (w.first = p_i)
-        elseif (w.first == j)
-            (s > 0) && (w.first = i)
-            # elseif (s < 0)
-            #     shift_origin!(w, s * origin_crosses)
-        end
-        #print("    Post origshift: ")
-        #printw(w)
     end
 end
 
@@ -93,21 +60,9 @@ function grove_coordinates(w)
     (a, b, c) .|> i -> w.values[i]
 end
 
-function printw(w::WrappyVector{N}) where {N}
-    #print("[")
-    i = w.first
-    for j ∈ 1:N
-        #print("$(w.values[i])")
-        #(j < N) && print(", ")
-        i = w.successors[i]
-    end
-    #println("]")
-end
-
 function solve_p1(nums)
     w = WrappyVector(nums)
     shift!(w)
-    printw(w)
     grove_coordinates(w) |> sum
 end
 
