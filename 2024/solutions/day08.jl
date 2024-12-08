c97a58c19e0e9fe93f031518aef7a392cf4d9616
Daniel González <daniel.glez.arribas@gmail.com>
2024-12-08 17:35:26 +0100

    Better D8

--- 2024/solutions/day08.jl
+++ 2024/solutions/day08.jl
@@ -46,3 +46,44 @@ end
 
 solve(M; part=:p1) |> println
 solve(M; part=:p2) |> println
+
+# version with one-pass though The Matrix
+# ---------------------------------------
+
+function add_antinodes!(M, antinodes, a, b, ::Val{:p1})
+    a_n = 2a - b
+    a_n ∈ CartesianIndices(M) && push!(antinodes, a_n)
+end
+
+function add_antinodes!(M, antinodes, a, b, ::Val{:p2})
+    step = a - b
+    x = a
+    while true
+        x ∈ CartesianIndices(M) ? push!(antinodes, x) : break
+        x += step
+    end
+end
+
+function solve_alt(M, part)
+    symbols = Dict{Char,Vector{CI{2}}}()
+    antinodes = Set{CI{2}}()
+    for ci ∈ CartesianIndices(M)
+        s = M[ci]
+        if s == '.'
+            continue
+        elseif s ∉ keys(symbols)
+            symbols[s] = [ci]
+        else
+            others = symbols[s]
+            for other ∈ others
+                add_antinodes!(M, antinodes, ci, other, part)
+                add_antinodes!(M, antinodes, other, ci, part)
+            end
+            push!(others, ci)
+        end
+    end
+    length(antinodes)
+end
+
+solve_alt(M, Val(:p1)) |> println
+solve_alt(M, Val(:p2)) |> println
